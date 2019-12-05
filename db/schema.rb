131f10ff6bfad77ab221e4e9ad6a81a2985e4cdf
maksimmanzhai <bender23max@gmail.com>
2019-12-05 23:17:39 +0300

    Add answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_201307) do
+ActiveRecord::Schema.define(version: 2019_12_05_201605) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
