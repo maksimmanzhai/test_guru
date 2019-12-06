43955c52412e9debffa59a4d66438c01f7c321a2
maksimmanzhai <bender23max@gmail.com>
2019-12-06 23:16:29 +0300

    Revert "Merge branch 'lesson03_2' into lesson03"
    This reverts commit 0e9e8298a0cdd9fda7c86344c1c9919a9a112e32, reversing
    changes made to 1219ced5b3910c0356972cb229073d581828c51c.

--- db/migrate/20191206193359_add_null_constraint.rb
+++
@@ -1,9 +0,0 @@
-class AddNullConstraint < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null(:tests, :title, false)
-    change_column_null(:tests, :level, false)
-    change_column_null(:questions, :body, false)
-    change_column_null(:answers, :title, false)
-    change_column_null(:users, :username, false)
-  end
-end
--- db/migrate/20191206194216_give_default_level_for_tests.rb
+++
@@ -1,5 +0,0 @@
-class GiveDefaultLevelForTests < ActiveRecord::Migration[6.0]
-  def change
-    change_column :tests, :level, :integer, default: 0
-  end
-end
