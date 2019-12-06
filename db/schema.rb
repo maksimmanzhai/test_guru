be6c5bf137759db31d50579950728b1606c12dc1
maksimmanzhai <bender23max@gmail.com>
2019-12-06 22:37:07 +0300

    Add default value to answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_193359) do
+ActiveRecord::Schema.define(version: 2019_12_06_193511) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "correct", default: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
