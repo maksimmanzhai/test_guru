e9e80cfc05a4f45a95eabc2bc8cb753b053683ab
maksimmanzhai <bender23max@gmail.com>
2019-12-09 20:02:46 +0300

    Edit request from Test table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def list_of_test(level)
-    CompletedTest.where("user_id = :user_id AND test_id = :test_id", user_id: self.id, test_id: level)
+    Test.where("level = ?", level)
   end
 end
