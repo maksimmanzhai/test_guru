a3f53168e919f9dbde39a105d6f2aefcddcc957d
maksimmanzhai <bender23max@gmail.com>
2019-12-11 23:46:14 +0300

    Edit method in_descending_order

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,11 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :answers, through: :questions
   has_many :completed_tests
+
+  scope :by_category, ->(category) { joins(:category).where(categories: { title: category }).order(title: :desc) }
+
+  def self.in_descending_order(category)
+    by_category(category).pluck(:title)
+  end
+
 end
