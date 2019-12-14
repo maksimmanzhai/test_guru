80e05c915af85f34e31eafcee9d867333d521607
maksimmanzhai <bender23max@gmail.com>
2019-12-14 22:09:00 +0300

    Edit relationships of tables

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category, touch: true
+  belongs_to :user, touch: true
   has_many :questions
   has_many :answers, through: :questions
   has_many :completed_tests
