e7c6a6226a24f3755ee505dabefd7a3ad95be919
maksimmanzhai <bender23max@gmail.com>
2019-12-16 21:11:19 +0300

    Add has_one references

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :completed_tests
   has_many :tests, through: :completed_tests
+  has_one :test
 
   def list_of_tests(level)
     self.tests.where("level = ?", level)
