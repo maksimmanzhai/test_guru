82aafa935c1a3772e29a054cdc9ef10ed01420ad
maksimmanzhai <bender23max@gmail.com>
2019-12-18 20:44:42 +0300

    Edit reference from user to test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :completed_tests
   has_many :tests, through: :completed_tests
-  has_many :tests, foreign_key: 'author_id'
-
+  has_many :author_tests, :class_name => 'Test', foreign_key: 'author_id'
+  
   def list_of_tests(level)
     self.tests.where("level = ?", level)
   end
