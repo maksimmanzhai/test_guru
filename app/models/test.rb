bd8ff7ec7b04b9c82ac6a9b483c76c7bbbd63507
maksimmanzhai <bender23max@gmail.com>
2019-12-18 22:09:25 +0300

    Add scopes for difficulty level

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,14 @@ class Test < ApplicationRecord
   has_many :answers, through: :questions
   has_many :completed_tests
 
+  scope :difficulty_level, ->(level) { where(level: level) }
+
+  scope :easy, -> { difficulty_level(0..1) }
+  scope :medium, -> { difficulty_level(2..4) }
+  scope :hard, -> { difficulty_level(5..Float::INFINITY) }
+
   def self.in_descending_order(category)
-    joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
+    joins(:category).where(categories: { title: category } ).order(title: :desc).pluck(:title)
   end
 
 end
