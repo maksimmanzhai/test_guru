cd6bbac1fcfa56cb105ad2b85a81e4251bc36899
maksimmanzhai <bender23max@gmail.com>
2019-12-20 20:56:40 +0300

    Add validation for Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,19 @@
 class Answer < ApplicationRecord
+  MAX_COUNT_OF_ANSWERS_TO_QUESTION = 4
+
   scope :correct_answer, -> { where(correct: true) }
 
   belongs_to :question, touch: true
 
   validates :title, presence: true
+  validate :answer_count, on: :create
+
+  private
+
+  def answer_count
+    if question.answers.count >= 4
+      errors.add(:MAX_COUNT_OF_ANSWERS_TO_QUESTION, 'There should be no more than 4 answers')
+    end
+  end
+
 end
