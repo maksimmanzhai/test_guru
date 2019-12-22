195b2a08e3465e35aa56ed532789960bf0c7a6a1
maksimmanzhai <bender23max@gmail.com>
2019-12-22 13:45:39 +0300

    Edit scope for correct answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,9 @@
 class Answer < ApplicationRecord
   MAX_COUNT_OF_ANSWERS_TO_QUESTION = 4
 
-  scope :correct_answer, -> { where(correct: true) }
-
   belongs_to :question, touch: true
+
+  scope :correct, -> { where(correct: true) }
 
   validates :title, presence: true
   validate :answer_count, on: :create
