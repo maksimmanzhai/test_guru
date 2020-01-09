0d8c4df16202d876c7ef3b4c77a24dabf4c98c36
maksimmanzhai <bender23max@gmail.com>
2020-01-09 21:52:50 +0300

    Fix errrors

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,21 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_question, only: %i[show, destroy]
-  before_action :find_test, only: %i[new, create, index]
+  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[new create index]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     @questions = @test.questions
   end
 
   def create
-    question = Question.create(question_params)
-    question.save
-    render plain: question.inspect
+    @question = Question.new(question_params)
+    if question.save
+      render plain: question.inspect
+    else
+      render plain: "Error. Try again"
+    end
   end
 
   def new
@@ -20,7 +23,6 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def destroy
@@ -42,7 +44,7 @@ class QuestionsController < ApplicationController
       params.require(:question).permit(:body, :test_id)
     end
 
-    def rescue_with_test_not_found
+    def rescue_with_question_not_found
       render plain: 'Question was not found'
     end
 
