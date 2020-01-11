a8d4ea9fa13f9c2b626d7e1bad2ea8be1117bf7a
maksimmanzhai <bender23max@gmail.com>
2020-01-11 03:15:08 +0300

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  get '/author', :to => redirect('/author.html')
-  get '/about', :to => redirect('/about.html')
+  get '/author', :to => redirect('public/author.html')
+  get '/about', :to => redirect('public/about.html')
 
   resources :tests do
     resources :questions, shallow: true
