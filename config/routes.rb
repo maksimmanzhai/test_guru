22093d12b032f38eea096a80e2ec383ce03ca75d
maksimmanzhai <bender23max@gmail.com>
2020-01-11 03:13:36 +0300

    Editing footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  get './about', to: 'public#about'
-  
+  get '/author', :to => redirect('/author.html')
+  get '/about', :to => redirect('/about.html')
+
   resources :tests do
     resources :questions, shallow: true
   end
