3a76d33d5626ef304c5f1fa1ed319d1e9b794207
maksimmanzhai <bender23max@gmail.com>
2020-01-13 02:44:04 +0300

    Add logic for completed tests

--- app/controllers/completed_tests_controller.rb
+++ app/controllers/completed_tests_controller.rb
@@ -1,4 +1,7 @@
 class CompletedTestsController < ApplicationController
+
+  before_action :set_completed_test, only: %i[show update result]
+
   def show
   end
 
@@ -6,5 +9,19 @@ class CompletedTestsController < ApplicationController
   end
 
   def update
+    @completed_test.accept!(params[:answer_ids])
+
+    if @completed_test.completed?
+      redirect_to result_completed_test_path(@completed_test)
+    else
+      render :show
+    end
   end
+
+  private
+
+  def set_completed_test
+    @completed_test = CompletedTest.find(params[:id])
+  end
+
 end
