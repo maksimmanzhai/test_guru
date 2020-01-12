75866b119300e081734c4cbe8d7ac95557c9fad8
maksimmanzhai <bender23max@gmail.com>
2020-01-12 15:55:04 +0300

    Destroy routes to public

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  get '/author', :to => redirect('public/author.html')
-  get '/about', :to => redirect('public/about.html')
-
   resources :tests do
     resources :questions, shallow: true
   end
