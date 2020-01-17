edf77c48742d579cd9752530259795de062c205d
maksimmanzhai <bender23max@gmail.com>
2020-01-17 14:45:18 +0300

    Add method authenticate for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :completed_tests
   has_many :tests, through: :completed_tests
@@ -11,6 +13,16 @@ class User < ApplicationRecord
 
   def completed_test(test)
     completed_tests.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 
 end
