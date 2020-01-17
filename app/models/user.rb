ffff50630ff289daeccad121ff7066870405f6eb
maksimmanzhai <bender23max@gmail.com>
2020-01-17 17:38:31 +0300

    Add gem bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :completed_tests
   has_many :tests, through: :completed_tests
   has_many :author_tests, :class_name => 'Test', foreign_key: 'author_id'
+
+  has_secure_password
 
   def list_of_tests(level)
     self.tests.where("level = ?", level)
