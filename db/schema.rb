143246c325c83b2b35c10ec4b0b194b870c420a7
maksimmanzhai <bender23max@gmail.com>
2020-01-17 14:40:35 +0300

    Add column password for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_191552) do
+ActiveRecord::Schema.define(version: 2020_01_17_113135) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_191552) do
     t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
