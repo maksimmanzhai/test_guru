0a0a06cd370248e7821557e53bec6374f40dc302
maksimmanzhai <bender23max@gmail.com>
2020-01-19 21:45:38 +0300

    Add log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
+  post :login, to: 'sessions#create'
+  delete :logout, to: 'sessions#destroy'
 
   resources :users, only: :create
   resources :sessions, only: :create
