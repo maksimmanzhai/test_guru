1a96d119954171e4384e93251484d295bd460ebd
maksimmanzhai <bender23max@gmail.com>
2020-01-25 18:34:29 +0300

    Edit redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       if cookies[:last_visited_path].present?
-        redirect_to cookies[:last_visited_path]
+        rediect_to cookies[:last_visited_path] || root_path
       else
         render :new
       end
