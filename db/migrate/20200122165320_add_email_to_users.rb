34b52462076f043ca9cd0672dee6243a4b1e2998
maksimmanzhai <bender23max@gmail.com>
2020-01-25 20:52:34 +0300

    Add migrate for devise

--- db/migrate/20200122165320_add_email_to_users.rb
+++ db/migrate/20200122165320_add_email_to_users.rb
@@ -1,11 +1,9 @@
 class AddEmailToUsers < ActiveRecord::Migration[6.0]
   def up
     add_column :users, :email, :string
-    add_index :users, :email
   end
 
   def down
-    remove_index :users, :email
     remove_column :users, :email, :string
   end
 end
