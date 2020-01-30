d1a1615195e5659db0d6f02453ccf61752af684a
maksimmanzhai <bender23max@gmail.com>
2020-01-30 20:31:01 +0300

    Set default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
   def after_sign_in_path_for(resource)
     if resource.class == Admin
-      admin_tests_path 
+      admin_tests_path
     elsif resource.class == User
-      root_path 
+      root_path
     end
   end
+
+  private
+
+    def set_locale
+      I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+    end
 
 end
