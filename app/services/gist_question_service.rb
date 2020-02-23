060517b748fe92b065cae3419c8d220141467107
maksimmanzhai <bender23max@gmail.com>
2020-02-23 22:19:11 +0300

    Add i18n to service

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -19,7 +19,7 @@ class GistQuestionService
 
   def gist_params
     {
-      description: "A question about #{@test.title} from TestGuru",
+      description: "#{I18n.t('.question_of_the_test', test_title: @test.title)}",
       files: {
         'test-guru-question.txt' => {
           content: gist_content
